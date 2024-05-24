325ebfed13056190c6adc77e51c2e9e13f3e54a6
KalyaniPatil <kalyanipatil2307@gmail.com>
2024-05-24 17:42:58 +0530

    pagination

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,14 +4,16 @@ import axios from 'axios'
 function Dashboard(){
     const [data, setData] = useState([]);
     const [month, setMonth] = useState("March");
+    const [numberOfPages, setNumberOfPages] = useState();
     const [page, setPage] = useState(1);
     const [search, setSearch] = useState("");
 
     useEffect(()=>{
         //GET Api data
-        axios.get(`http://localhost:5000/all`, {params: { search: search}})
+        axios.get(`http://localhost:5000/all`, {params: { search: search, page: page}})
         .then((res) => {
-            setData(res.data);
+            setData(res.data.result);
+            setNumberOfPages(res.data.numberOfPages)
         })
         .catch((err)=>console.log(err))
     }, [page, month, search])
@@ -25,7 +27,7 @@ function Dashboard(){
             <div className="row">
                 <div className="col-6">
                     <form onSubmit={searchData}>
-                    <input type="text" placeholder="Search Transactions" />
+                    <input type="search" placeholder="Search Transactions" onChange={(event)=>{if(event.target.value == "") setSearch("")}} />
                     </form>
                 </div>
             </div>
@@ -59,6 +61,20 @@ function Dashboard(){
                     }
                 </tbody>
             </table>
+            <nav aria-label="Page navigation example">
+                <div className="float-end">
+                    <h6>Per Page : {data.length}</h6>
+                </div>
+                <ul class="pagination justify-content-center">
+                    <li className={page > 1 ? "page-item" : "page-item disabled"}>
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true" onClick={()=>{if(page > 1) setPage(page-1)}}>Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">{page}</a></li>
+                    <li className={page < numberOfPages ? "page-item" : "page-item disabled"}>
+                        <a className="page-link" href="#" onClick={()=>{if(page < numberOfPages) setPage(page+1)}}>Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 const port = 5000
-
+const productsPerPage = 10;
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -50,24 +50,34 @@ function getExternalData() {
         })
     })
 }
-// app.get('/all', function(req, resp){
-//         db.query(`SELECT * FROM products`, (error,result,fields)=>{
-//             if(error) throw error
-//             resp.send(result)
-//         })
-// })
 
 app.get('/all', function(req, resp){
     const data = req.query.search
     if(data){
         db.query(`SELECT * FROM products WHERE title LIKE '%${data}%' OR description LIKE '%${data}%' OR price LIKE '%${data}%'`, (error,result,fields)=>{
-            if(error) throw error
-            resp.send(result)
+            if(error){
+                throw error
+            }
+            const numberOfProducts = result.length;
+            const numberOfPages = Math.ceil(numberOfProducts / productsPerPage);
+            let page = req.query.page;
+            const strtingLimit = (page - 1) * productsPerPage;
+            db.query(`SELECT * FROM products WHERE title LIKE '%${data}%' OR description LIKE '%${data}%' OR price LIKE '%${data}%' LIMIT ${strtingLimit}, ${productsPerPage}`, (error, products) => {
+                resp.send({"result" : products, "numberOfPages": numberOfPages})
+            })
         })
     }else{
         db.query(`SELECT * FROM products`, (error,result,fields)=>{
-            if(error) throw error
-            resp.send(result)
+            if(error){
+                throw error
+            }
+            const numberOfProducts = result.length;
+            const numberOfPages = Math.ceil(numberOfProducts / productsPerPage);
+            let page = req.query.page;
+            const strtingLimit = (page - 1) * productsPerPage;
+            db.query(`SELECT * FROM products LIMIT ${strtingLimit}, ${productsPerPage}`, (error, products) => {
+                resp.send({"result" : products, "numberOfPages": numberOfPages})
+            })
         })
     }
 })
